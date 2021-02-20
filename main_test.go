79d97cec4c69dfacfd470ff543a2ff54740f8553
Marten Seemann <martenseemann@gmail.com>
2021-02-20 10:24:37 +0800

    add a test workflow and a long lasting test case

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestRandomSleep(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	d := time.Duration(10+rand.Intn(20)) * time.Second
+	fmt.Println("Sleeping for", d)
+	time.Sleep(d)
+}
